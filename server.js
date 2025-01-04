0c5a20915f5ee4012bc32c33485fa873f365c954
Ashitom <sasukeuchia32828@gmail.com>
2025-01-04 15:19:48 +0545

    Basic Server Side Rendering With React And Node

---
+++ server.js
@@ -0,0 +1,90 @@
+const { readFileSync } = require("fs");
+const { createServer } = require("http");
+const { parse } = require("url");
+const { renderToString } = require("react-dom/server");
+const React = require("react");
+
+const pizzas = [
+  {
+    name: "Focaccia",
+    price: 6,
+  },
+  {
+    name: "Pizza Margherita",
+    price: 10,
+  },
+  {
+    name: "Pizza Spinaci",
+    price: 12,
+  },
+  {
+    name: "Pizza Funghi",
+    price: 12,
+  },
+  {
+    name: "Pizza Prosciutto",
+    price: 15,
+  },
+];
+
+function Home() {
+  return (
+    <div>
+      <h1>🍕 Fast React Pizza Co.</h1>
+      <p>This page has been rendered with React on the server 🤯</p>
+
+      <h2>Menu</h2>
+      <ul>
+        {pizzas.map((pizza) => (
+          <MenuItem pizza={pizza} key={pizza.name} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Counter() {
+  const [count, setCount] = React.useState(0);
+  return (
+    <div>
+      <button onClick={() => setCount((c) => c + 1)}>+1</button>
+      <span>{count}</span>
+    </div>
+  );
+}
+
+function MenuItem({ pizza }) {
+  return (
+    <li>
+      <h4>
+        {pizza.name} (${pizza.price})
+      </h4>
+      <Counter />
+    </li>
+  );
+}
+
+const htmlFile = readFileSync(`${__dirname}/index.html`, "utf-8");
+
+const server = createServer((request, response) => {
+  const pathname = parse(request.url, true).pathname;
+
+  if (pathname === "/") {
+    const renderHtml = renderToString(<Home />);
+    const html = htmlFile.replace("CONTENT", renderHtml);
+
+    response.writeHead(200, {
+      "content-type": "text/html",
+    });
+
+    //  response.end(htmlFile);
+
+    response.end(html);
+  } else if (pathname === "/login") {
+    response.end("Prepare to Log In");
+  } else {
+    response.end("Page Not Found");
+  }
+});
+
+server.listen(8000, () => console.log("Listening on Port 8000"));
